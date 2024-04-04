b901ae19edf0995fe58fc0a3a3619f668ae40130
Josh <josh.e.stein@gmail.com>
2024-04-04 17:55:02 -0400

    Render time blocks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,15 +10,46 @@
 	endTime.setHours(17, 0, 0);
 
 	let timeDelta = 60;
+	let timeBlocks: {start: Date, end: Date}[] = [];
+
+	$: {
+		timeBlocks = [];
+		let currentTime = new Date(startTime);
+		while (currentTime <= endTime) {
+			const start = new Date(currentTime);
+			currentTime.setMinutes(currentTime.getMinutes() + timeDelta);
+			timeBlocks.push({start, end: new Date(currentTime)});
+		}
+	}
+
 </script>
 
 <h1>Time Blocker</h1>
+<h5>What do you want to get done?</h5>
 <Settings bind:startTime bind:endTime bind:timeDelta />
-<ul>
-	<!--{#each timeBlocks as tb}-->
-	<!--    <FormRow></FormRow>-->
-	<!--{/each}-->
-</ul>
+<div id="time-blocks">
+	{#each timeBlocks as tb}
+		<FormRow startTime={tb.start} endTime={tb.end}/>
+	{/each}
+</div>
 
 <style>
+	h1 {
+			margin-bottom: 0;
+	}
+
+	h5 {
+			margin-top: 0.25rem;
+      color: var(--accent);
+	}
+
+	#time-blocks {
+			display: flex;
+			flex-flow: column wrap;
+			gap: 0.5rem;
+			padding: 1rem;
+			margin-top: 0.375rem;
+			max-width: 340px;
+			width: 100%;
+	}
 </style>
